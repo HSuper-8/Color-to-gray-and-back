7a1cd199f059fb1efc12095c8c68cf1e83eab7a5
Felipe Lima <felipesoueu@live.com>
2018-09-18 10:46:38 -0300

    Bug na simulacao corrigido

--- Simulation.py
+++ Simulation.py
@@ -23,11 +23,11 @@ def errorDiffusion(image):
                 imageExp[x, y] = 255
             else:
                 imageExp[x, y] = 0
-                quantError = oldPixel - imageExp[x, y]
-                imageExp[x + 1, y] = imageExp[x + 1, y] + 7 / 16.0 * quantError
-                imageExp[x - 1, y + 1] = imageExp[x - 1, y + 1] + 3 / 16.0 * quantError
-                imageExp[x, y + 1] = imageExp[x, y + 1] + 5 / 16.0 * quantError
-                imageExp[x + 1, y + 1] = imageExp[x + 1, y + 1] + 1 / 16.0 * quantError
+            quantError = oldPixel - imageExp[x, y]
+            imageExp[x + 1, y] = imageExp[x + 1, y] + 7 / 16.0 * quantError
+            imageExp[x - 1, y + 1] = imageExp[x - 1, y + 1] + 3 / 16.0 * quantError
+            imageExp[x, y + 1] = imageExp[x, y + 1] + 5 / 16.0 * quantError
+            imageExp[x + 1, y + 1] = imageExp[x + 1, y + 1] + 1 / 16.0 * quantError
 
     # Removes the border previously inserted
     image[:, :] = imageExp[1:image.shape[0] + 1, 1:image.shape[1] + 1]
